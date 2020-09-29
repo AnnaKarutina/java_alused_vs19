1f2378230a0a5f8d163abba5dc246e3aceb26564
Anna Karutina <anna.karutina@khk.ee>
2020-09-29 10:06:32 +0300

    Exercise 39.2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         printStars(5);
         printStars(3);
         printStars(9);
+        System.out.println();
+        printSquare(4);
     }
     private static void printStars(int amount) {
         int i = 0;
@@ -15,5 +17,17 @@ public class Main {
             i++;
         }
         System.out.println();
+    }
+    private static void printSquare(int sideSize){
+        int i = 0;
+        while(i < sideSize) {
+            int j = 0;
+            while (j < sideSize) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
     }
 }

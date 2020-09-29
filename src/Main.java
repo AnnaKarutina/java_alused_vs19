6f3c95e7579d483d2c5b8cf8c168f704ffdae87c
Anna Karutina <anna.karutina@khk.ee>
2020-09-29 10:41:47 +0300

    Exercise 41.1 and 41.2

--- src/Main.java
+++ src/Main.java
@@ -4,58 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        printStars(5);
-        printStars(3);
-        printStars(9);
-        System.out.println();
-        printSquare(4);
-        System.out.println();
-        printRectangle(17,3);
-        System.out.println();
-        printTriangle(4);
+        drawNumber();
     }
-    private static void printStars(int amount) {
-        int i = 0;
-        while(i < amount){
-            System.out.print("*");
-            i++;
-        }
-        System.out.println();
-    }
-    private static void printSquare(int sideSize){
-        int i = 0;
-        while(i < sideSize) {
-            int j = 0;
-            while (j < sideSize) {
-                System.out.print("*");
-                j++;
+    private static void drawNumber(){
+        int number = (int)(Math.random()*101);
+        System.out.println(number);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a number: ");
+        int guess = Integer.parseInt(input.nextLine());
+        while(number != guess){
+            if(number > guess){
+                System.out.println("The number is greater");
+            } else if(number < guess){
+                System.out.println("The number is lesser");
             }
-            i++;
-            System.out.println();
+            System.out.println("Guess a number: ");
+            guess = Integer.parseInt(input.nextLine());
         }
-    }
-    private static void printRectangle(int width, int height){
-        int h = 0;
-        while(h < height){
-            int w = 0;
-            while(w < width){
-                System.out.print("*");
-                w++;
-            }
-            h++;
-            System.out.println();
-        }
-    }
-    private static void printTriangle(int size){
-        int row = 0;
-        while(row < size){
-            int width = 0;
-            while(width <= row){
-                System.out.print("*");
-                width++;
-            }
-            row++;
-            System.out.println();
-        }
+        System.out.println("Congratulations, your guess is correct!");
+
     }
 }

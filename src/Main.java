89fd1fccf2e0bb2e5f94df33e553c26792fa6c75
Anna Karutina <anna.karutina@khk.ee>
2020-09-29 09:55:10 +0300

    Exercise 36.3

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,14 @@ public class Main {
         System.out.println("Type numbers: ");
         int number = Integer.parseInt(reader.nextLine());
         int sum = 0;
+        int count = 0;
         while (number != -1) {
             sum += number;
+            count++;
             number = Integer.parseInt(reader.nextLine());
         }
         System.out.println("Thank you and see you later!");
         System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + count);
     }
 }

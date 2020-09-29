0594358360fde31629209c6ea79047d7403cb21a
Anna Karutina <anna.karutina@khk.ee>
2020-09-29 09:52:43 +0300

    Exercise 36.1

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner reader = new Scanner(System.in);
-        System.out.print("Until what? ");
+        System.out.println("Type numbers: ");
         int number = Integer.parseInt(reader.nextLine());
-        int i = 1;
-        int sum = 0;
-        while (i <= number) {
-            sum += i;
-            i++;
+        while (number != -1) {
+            number = Integer.parseInt(reader.nextLine());
         }
-        System.out.println("Sum is " + sum);
+        System.out.println("Thank you and see you later!");
     }
 }

b63af0564712a3bbc6584e81370c29caeec2cb24
Anna Karutina <anna.karutina@khk.ee>
2020-09-29 09:53:57 +0300

    Exercise 36.2

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,12 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         System.out.println("Type numbers: ");
         int number = Integer.parseInt(reader.nextLine());
+        int sum = 0;
         while (number != -1) {
+            sum += number;
             number = Integer.parseInt(reader.nextLine());
         }
         System.out.println("Thank you and see you later!");
+        System.out.println("The sum is " + sum);
     }
 }

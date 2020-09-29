b8af6accddfe5b18c21810eb4596f8e4b5085e5d
Anna Karutina <anna.karutina@khk.ee>
2020-09-29 09:13:07 +0300

    Exercise 32

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner reader = new Scanner(System.in);
-        System.out.print("Up to what number? ");
+        System.out.print("Until what? ");
         int number = Integer.parseInt(reader.nextLine());
         int i = 1;
+        int sum = 0;
         while (i <= number) {
-            System.out.println(i);
+            sum += i;
             i++;
         }
+        System.out.println("Sum is " + sum);
     }
 }

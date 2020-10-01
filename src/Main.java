3bc7d4303bba528cbf78664fe5edd0410b1e9e8c
Anna Karutina <anna.karutina@khk.ee>
2020-10-01 12:26:49 +0300

    Exercise 44

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        drawNumber();
+        int answer =  least(7, 2);
+        System.out.println("Least: " + answer);
     }
-    private static void drawNumber(){
-        int number = (int)(Math.random()*101);
-        System.out.println(number);
-        Scanner input = new Scanner(System.in);
-        System.out.println("Guess a number: ");
-        int guess = Integer.parseInt(input.nextLine());
-        int guessCount = 1;
-        while(number != guess){
-            if(number > guess){
-                System.out.println("The number is greater, guesses made: " + guessCount);
-            } else if(number < guess){
-                System.out.println("The number is lesser, guesses made: " + guessCount);
-            }
-            System.out.println("Guess a number: ");
-            guess = Integer.parseInt(input.nextLine());
-            guessCount++;
-        }
-        System.out.println("Congratulations, your guess is correct!");
 
+    public static int least(int number1, int number2) {
+        // write program code here
+        int leastNumber;
+        if(number1 < number2){
+            leastNumber = number1;
+        } else if(number1 > number2){
+            leastNumber = number2;
+        } else{
+            leastNumber = number1;
+        }
+        // method needs a return in the end
+        return leastNumber;
     }
 }

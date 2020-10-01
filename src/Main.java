54271a4495651e0298c632d282bb49edbab990fb
Anna Karutina <anna.karutina@khk.ee>
2020-10-01 12:34:13 +0300

    Exercise 50

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static double average(int number1, int number2, int number3, int number4) {
-        // write your code here
-        int sum = number1 + number2 + number3 + number4;
-        double average = (double)sum / 4;
-        return average;
-    }
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = input.nextLine();
+        int i = 0;
+        if(name.length() >= 3){
+            while(i < 3){
+                System.out.println((i+1) + ". character: " + name.charAt(i));
+                i++;
+            }
+        }
 
-    public static void main(String[] args) {
-        double answer = average(4, 3, 6, 1);
-        System.out.println("average: " + answer);
     }
 
 }

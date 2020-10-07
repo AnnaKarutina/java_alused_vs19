ae59725fb211a1cca2ec6ae604febb28f87987b1
Anna Karutina <anna.karutina@khk.ee>
2020-10-07 12:43:44 +0300

    Exercise 72.3

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Main {
 
      public static void main(String[] args) {
-         Account myAccount = new Account("My account",0.00);
-         Account mattAccount = new Account("Matt's account",1000.00);
+         Account aAccount = new Account("A",100.00);
+         Account bAccount = new Account("B",0.00);
+         Account cAccount = new Account("C",0.00);
 
          System.out.println("Initial state");
-         System.out.println(myAccount);
-         System.out.println(mattAccount);
+         System.out.println(aAccount);
+         System.out.println(bAccount);
+         System.out.println(cAccount);
 
-         // transfer(mattAccount, myAccount, 100);
-         mattAccount.transfer(myAccount, 100);
+         transfer(aAccount, bAccount, 50);
+         transfer(bAccount, cAccount, 25);
 
          System.out.println("Final state");
-         System.out.println(myAccount);
-         System.out.println(mattAccount);
+         System.out.println(aAccount);
+         System.out.println(bAccount);
+         System.out.println(cAccount);
+
     }
 
     public static void transfer(Account from, Account to, double amount){

4e8f75b0538aa28a8a2f776b829fbbab6b1002dc
Anna Karutina <anna.karutina@khk.ee>
2020-10-07 12:32:00 +0300

    Exercise 72.1

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,16 @@ import java.util.Scanner;
 public class Main {
 
      public static void main(String[] args) {
-         Account bartosAccount = new Account("Barto's account",100.00);
-         Account bartosSwissAccount = new Account("Barto's account in Switzerland",1000000.00);
+         Account myAccount = new Account("Anna's account",100.00);
 
          System.out.println("Initial state");
-         System.out.println(bartosAccount);
-         System.out.println(bartosSwissAccount);
+         System.out.println(myAccount);
 
-         bartosAccount.withdrawal(20);
-         System.out.println("Barto's account balance is now: " + bartosAccount.balance());
-         bartosSwissAccount.deposit(200);
-         System.out.println("Barto's Swiss account balance is now: " + bartosSwissAccount.balance());
+         myAccount.deposit(20);
+         System.out.println("My account balance is now: " + myAccount.balance());
 
          System.out.println("Final state");
-         System.out.println(bartosAccount);
-         System.out.println(bartosSwissAccount);
+         System.out.println(myAccount);
     }
 
 }

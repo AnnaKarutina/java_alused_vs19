df4974669412944c79ca889bd6264c440fa156a7
Anna Karutina <anna.karutina@khk.ee>
2020-10-07 12:37:52 +0300

    Exercise 72.1 - transfer in Main class

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class Main {
 
      public static void main(String[] args) {
-         Account myAccount = new Account("Anna's account",100.00);
+         Account myAccount = new Account("My account",0.00);
+         Account mattAccount = new Account("Matt's account",1000.00);
 
          System.out.println("Initial state");
          System.out.println(myAccount);
+         System.out.println(mattAccount);
 
-         myAccount.deposit(20);
-         System.out.println("My account balance is now: " + myAccount.balance());
+         transfer(mattAccount, myAccount, 100);
 
          System.out.println("Final state");
          System.out.println(myAccount);
+         System.out.println(mattAccount);
+    }
+
+    public static void transfer(Account from, Account to, double amount){
+         from.withdrawal(amount);
+         to.deposit(amount);
     }
 
 }

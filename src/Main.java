54f87998bcf4d4a35efb60882e4e95491ee7e8be
Anna Karutina <anna.karutina@khk.ee>
2020-10-07 12:55:53 +0300

    Exercise 73

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,8 @@ import java.util.Scanner;
 public class Main {
 
      public static void main(String[] args) {
-         Account aAccount = new Account("A",100.00);
-         Account bAccount = new Account("B",0.00);
-         Account cAccount = new Account("C",0.00);
-
-         System.out.println("Initial state");
-         System.out.println(aAccount);
-         System.out.println(bAccount);
-         System.out.println(cAccount);
-
-         transfer(aAccount, bAccount, 50);
-         transfer(bAccount, cAccount, 25);
-
-         System.out.println("Final state");
-         System.out.println(aAccount);
-         System.out.println(bAccount);
-         System.out.println(cAccount);
-
-    }
-
-    public static void transfer(Account from, Account to, double amount){
-         from.withdrawal(amount);
-         to.deposit(amount);
+         Product product = new Product("Banana", 1.1, 13);
+         product.printProduct();
     }
 
 }

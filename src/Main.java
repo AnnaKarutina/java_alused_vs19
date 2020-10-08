f9b567631f67cdda40900a7a5d98425fd6a67944
Anna Karutina <anna.karutina@khk.ee>
2020-10-08 15:41:58 +0300

    Exercise 79

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,24 @@ import java.util.Scanner;
 public class Main {
 
      public static void main(String[] args) throws Exception {
-         BoundedCounter seconds = new BoundedCounter(59);
-         BoundedCounter minutes = new BoundedCounter(59);
-         BoundedCounter hours = new BoundedCounter(23);
+         NumberStatistics allNumbers = new NumberStatistics();
+         NumberStatistics evenNumbers = new NumberStatistics();
+         NumberStatistics oddNumbers = new NumberStatistics();
 
          Scanner reader = new Scanner(System.in);
-
-         System.out.print("seconds: ");
-         int s = Integer.parseInt(reader.nextLine()); // read the initial value of seconds from the user
-         System.out.print("minutes: ");
-         int m = Integer.parseInt(reader.nextLine());// read the initial value of minutes from the user
-         System.out.print("hours: ");
-         int h = Integer.parseInt(reader.nextLine());// read the initial value of hours from the user
-
-         seconds.setValue(s);
-         minutes.setValue(m);
-         hours.setValue(h);
-
-         while ( true ) {
-             System.out.println( hours + ":" + minutes + ":" + seconds );
-             Thread.sleep(1000);
-             // like in previous but seconds taken into account
-             seconds.next();
-             if(seconds.getValue() == 0){
-                 minutes.next();
-                 if(minutes.getValue() == 0){
-                     hours.next();
-                 }
+         System.out.println("Type numbers:");
+         int number = Integer.parseInt(reader.nextLine());
+         while(number != -1){
+             allNumbers.addNumber(number);
+             if(number % 2 == 0){
+                 evenNumbers.addNumber(number);
+             } else {
+                 oddNumbers.addNumber(number);
              }
-
-             //i++;
+             number = Integer.parseInt(reader.nextLine());
          }
+         System.out.println("sum: " + allNumbers.sum());
+         System.out.println("sum of even: " + evenNumbers.sum());
+         System.out.println("sum of odd: " + oddNumbers.sum());
     }
 }

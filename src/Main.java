1f4b3b14b073c9861d40b9d8c31caa5b8d093564
Anna Karutina <anna.karutina@khk.ee>
2020-10-12 11:27:08 +0300

    Exercise 100.2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         NightSky NightSky = new NightSky(0.1, 40, 10);
-        NightSky.printLine();
+        NightSky.print();
         //System.out.println("Number of stars: " + NightSky.starsInLastPrint());
         System.out.println("");
 
         NightSky = new NightSky(0.2, 15, 6);
-        NightSky.printLine();
+        NightSky.print();
         //System.out.println("Number of stars: " + NightSky.starsInLastPrint());
     }
 }
--- src/NightSky.java
+++ src/NightSky.java
@@ -33,4 +33,10 @@ public class NightSky {
         }
         System.out.println();
     }
+
+    public void print(){
+        for(int i = 0; i < this.height; i++){
+            this.printLine();
+        }
+    }
 }
